40e08a10f94a123f8936e6f56dc8e1ccc7c40429
Zhannurkhan <zhannurkhan04@gmail.com>
2022-12-10 15:45:20 +0600

    updated colors

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
-const slides = document.querySelectorAll(".slide");
 
-slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${indx * 100}%)`;
-});
-const nextSlide = document.querySelector(".btn-next");
-
-let curSlide = 0;
-let maxSlide = slides.length - 2;
-
-nextSlide.addEventListener("click", function () {
-    if (curSlide === maxSlide) {
-        curSlide = 0;
-    } else {
-        curSlide++;
-    }
-
-    slides.forEach((slide, indx) => {
-        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-    });
-});
-
-const prevSlide = document.querySelector(".btn-prev");
-
-prevSlide.addEventListener("click", function () {
-    if (curSlide === 0) {
-        curSlide = maxSlide;
-    } else {
-        curSlide--;
-    }
-
-    slides.forEach((slide, indx) => {
-        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-    });
-});
-
+var course = "";
+var backgroundColor = "";
+var img = "";
 function changePage(element) {
-
-    console.log( element.getElementsByTagName("h2")[0].innerHTML)
-
-
+    course = element.getElementsByTagName("h2")[0].innerHTML;
+    backgroundColor = element.style.backgroundColor;
+    img = element.getElementsByTagName("img")[0].src;
     window.location.href = 'course.html';
-    console.log(document.getElementById("course-name").innerHTML);
-    console.log(document.getElementById("about-course").style.backgroundColor);
 }
+
+function whenLoad() {
+    console.log(course);
+    document.getElementById("course-name").innerHTML = "Därıs" + " " + course;
+
+}
+
+
+
+    const slides = document.querySelectorAll(".slide");
+
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${indx * 100}%)`;
+    });
+    const nextSlide = document.querySelector(".btn-next");
+
+    let curSlide = 0;
+    let maxSlide = slides.length - 2;
+
+    nextSlide.addEventListener("click", function () {
+        if (curSlide === maxSlide) {
+            curSlide = 0;
+        } else {
+            curSlide++;
+        }
+
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+    });
+
+    const prevSlide = document.querySelector(".btn-prev");
+
+    prevSlide.addEventListener("click", function () {
+        if (curSlide === 0) {
+            curSlide = maxSlide;
+        } else {
+            curSlide--;
+        }
+
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+    });
 
 
