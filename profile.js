840438487a4fa55f2af7acb31aabb5077192943b
Zhannurkhan <zhannurkhan04@gmail.com>
2022-12-20 15:38:58 +0600

    back

--- profile.js
+++ profile.js
@@ -1,10 +1,13 @@
-var name = "";
-var surname = "";
-var age = 0;
-var about = "";
-var insta = "";
-var password = "";
+var name = "Kuanyshbek";
+var surname = "Mamay";
+var age = 23;
+var about = "Fromt-end lecturer";
+var insta = "https://www.instagram.com/kuka_uragan/";
 
+document.getElementById("aboutMe").innerHTML = about;
+document.getElementById("nameUser").innerHTML = name + " " + surname;
+document.getElementById("userAge").innerHTML = age.toString();
+document.getElementById("userInsta").href = insta;
 function rega(uname,usurname,uemail,uage,uabout,uinsta,upassword,upasswordAgain) {
     if(upassword === upasswordAgain) {
         $.ajax({
@@ -73,22 +76,37 @@ function swapToReg() {
     },"slow");
 }
     function logIn(uemail, upass) {
-        alert(uemail + " " + upass);
+
       $.ajax({
         url: 'log.php',
         type: 'POST',
         data: {email: uemail, password: upass},
         success: function (jsonData){
-          alert(jsonData);
+
               if(jsonData !== "Error"){
               var json = JSON.parse(jsonData);
-              alert(json);
+
               name = json[0]['surname'];
               surname= json[0]['surname'];
-              about = json[0]['about_user	'];    
+              about = json[0]['about_user'];
               age = json[0]['age'];
-              insta = json[0]['user_insta'] ;
-              swapToProfile();
+              insta = json[0]['user_insta'];
+
+                document.getElementById("aboutMe").innerHTML = about;
+                document.getElementById("nameUser").innerHTML = name + " " + surname;
+                document.getElementById("userAge").innerHTML = age.toString();
+                document.getElementById("userInsta").href = insta;
+                  $("#signin").hide();
+                  $("#signin").animate({left: '-=100px',
+                  },0);
+                  $("#signin").animate({left: '+=100px',
+                  },"slow");
+
+                  $("#mainProfile").show();
+                  $("#mainProfile").animate({left: '-=100px',
+                  },0);
+                  $("#mainProfile").animate({left: '+=100px',
+                  },"slow");
             }else {
               alert("UPS");
             }
@@ -96,8 +114,5 @@ function swapToReg() {
         error: function (){}
         });
     }
-document.getElementById("aboutMe").innerHTML = about;
-document.getElementById("nameUser").innerHTML = name + " " + surname;
-document.getElementById("userAge").innerHTML = age.toString();
-document.getElementById("userInsta").href = insta;
+
 
