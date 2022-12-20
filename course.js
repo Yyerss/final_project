24e14a20caaa7461b825cfdd35094b691e43c57f
Ersultan <ekosh66@gmail.com>
2022-12-20 11:31:01 +0600

    button jazylu

--- course.js
+++ course.js
@@ -16,5 +16,30 @@ var img = "";
                     document.getElementById('courseImg').src = img;
                 }
            });
+
+function buy(pochta) {
+    $("#container-buy").hide();
+    $("#container-buy").animate({left: '-=100px',
+    },0);
+    $("#container-buy").animate({left: '+=100px',
+    },"slow");
+    alert("Sızge jaqyn arada " + pochta + " pochtasyna habarlama keledı!");
+}
+
+function jazyl() {
+    $("#container-buy").show();
+    $("#container-buy").animate({left: '-=100px',
+    },0);
+    $("#container-buy").animate({left: '+=100px',
+    },"slow");
+}
+
+function jabu() {
+    $("#container-buy").hide();
+    $("#container-buy").animate({left: '-=100px',
+    },0);
+    $("#container-buy").animate({left: '+=100px',
+    },"slow");
+}
         
     
